40045c5709aa03035dc171cca62eb23ac837126c
deploy.sh <passyt@passyt-pc>
2018-04-28 19:25:58 +0800

    publish: Add changelog for 2.2
    generated from commit 2245e95e485f9a2e583890a118aadc1bab964eac

--- p.sh
+++
@@ -1,5 +0,0 @@
-scp -i go-spec.pem  supplier/supplier-api.html.md distributor/distributor-api.html.md distributor/distributor-xml-api.html.md owen@go-spec.derbysoftapi.com:/usr/local/applications/supplier/source/
-
-scp -i go-spec.pem distributor/_faq.md distributor/_code.table.md distributor/_speciality.md owen@go-spec.derbysoftapi.com:/usr/local/applications/supplier/source/includes 
-
-scp -i go-spec.pem index.html.md owen@go-spec.derbysoftapi.com:/usr/local/applications/supplier/source/ 
